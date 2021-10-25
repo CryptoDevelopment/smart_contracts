47857bd29b74a8b02d476d379c4b3ba9393973df
Boris <boris@example.com>
2021-10-25 22:44:07 +0200

    shorted variables list

---
+++ variables.sol
@@ -0,0 +1,13 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+// learning variable types
+
+
+// three main types of variable: Booleans, INtegers & Strings
+
+contract learnVariables {
+    uint chockleteBar = 10;
+    uint storeOwner = 300;
+    bool lieDetector = true;
+    string errorMessageText = "Error! There has been a mistake 🙁";
+}
