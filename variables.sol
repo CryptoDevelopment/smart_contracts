aea98b2043405978a445094188e90fa6819886d3
grigacs <grigacs98@gmail.com>
2021-10-25 23:43:30 +0200

    merge

--- variables.sol
+++ variables.sol
@@ -3,17 +3,12 @@ pragma solidity >= 0.7.0 < 0.9.0;
 // learning variable types
 
 
-<<<<<<< HEAD
 // three main types of variable: Booleans, Integers & Strings
-=======
-// three main types of variable: Booleans, INtegers & Strings
->>>>>>> main
 
 contract learnVariables {
     uint chockleteBar = 10;
     uint storeOwner = 300;
     bool lieDetector = true;
-<<<<<<< HEAD
     string errorMessageText = "Error! There has been a mistake :(";
     
     uint totalValue = chockleteBar + storeOwner;
@@ -24,7 +19,5 @@ contract learnVariables {
     
     spend = false;
     notifySend = "you have spent money";
-=======
     string errorMessageText = "Error! There has been a mistake 🙁";
->>>>>>> main
 }
